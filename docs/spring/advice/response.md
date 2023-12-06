3bbb3e969757374dfd6dec4be3e9df16a465aa1b
SunYufei <sunyufei963@qq.com>
2023-12-06 22:12:34 +0800

    [spring boot] 全局异常处理，统一返回结果

---
+++ docs/spring/advice/response.md
@@ -0,0 +1,3 @@
+# 统一返回格式
+
+<<< @/../spring-boot-demo/controller/src/main/java/ml/sun/common/advice/ResponseAdvice.kt
--- spring-boot-demo/test/src/test/java/ml/sun/controller/HealthControllerTest.kt
+++ spring-boot-demo/test/src/test/java/ml/sun/controller/HealthControllerTest.kt
@@ -1,8 +1,10 @@
 package ml.sun.controller
 
+import ml.sun.common.result.ResultCode
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
@@ -21,6 +23,29 @@ class HealthControllerTest {
         mockMvc.get("/health?param=test").andExpect {
             status { isOk() }
             jsonPath("$.data") { value("test") }
+        }.andDo { print() }
+    }
+
+    @Test
+    fun error() {
+        mockMvc.get("/health/error").andExpect {
+            status { isOk() }
+            jsonPath("$.success") { value(false) }
+            jsonPath("$.code") { value(ResultCode.EXCEPTION.code) }
+            jsonPath("$.message") { value(ResultCode.EXCEPTION.message) }
+        }.andDo { print() }
+    }
+
+    @Test
+    fun body() {
+        mockMvc.get("/health/body?code=100").andExpect {
+            status { isOk() }
+            jsonPath("$.data") { value(100) }
+        }.andDo { print() }
+
+        mockMvc.get("/health/html").andExpect {
+            status { isOk() }
+            content { contentType("${MediaType.TEXT_HTML_VALUE};charset=UTF-8") }
         }.andDo { print() }
     }
 }
